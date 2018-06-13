97c79f51df03074390c989ddf61a3c43697c3707
Allanlychee <allanvly@gmail.com>
2018-06-12 19:54:23 -0700

    reactrouterpt2

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import AppRouter from './components/approuter'
 import axios from 'axios'
+import './App.css'
 
 class App extends Component {
   componentDidMount () {
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,16 +3,18 @@ import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = props => (
-  <nav>
-    <div className='nav-wrapper blue darken-4' >
-      <a className='brand-logo'>Real Estate {props.statusMessage}</a>
-      <ul id='nav-mobile' className='right hide-on-med-and-down'>
+  <div className = 'navbar-fixed transparent'>
+  <nav className='transparent'>
+    <div className='nav-wrapper transparent'>
+      <a className='brand-logo center'>Real Estate {props.statusMessage}</a>
+      <ul id='nav-mobile' className='right hide-on-med-and-down transparent'>
         <li><Link to='/'>Main</Link></li>
         <li><Link to='/info'>Info</Link></li>
         <li><Link to='/contact'>Contact</Link></li>
       </ul>
     </div>
   </nav>
+  </div>
 )
 
 export default Navbar;
