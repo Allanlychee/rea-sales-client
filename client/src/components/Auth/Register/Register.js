54184ce867e4137d3f395da594b6e2b5d29496b2
Allanlychee <allanvly@gmail.com>
2018-06-21 19:41:20 -0700

    addseller

--- client/src/components/Auth/Register/Register.js
+++ client/src/components/Auth/Register/Register.js
@@ -9,22 +9,16 @@ class Register extends Component {
       <form class="form-signin">
         <h1 class="h3 mb-3 font-weight-normal">Please register</h1>
         <label for="inputFirst" class="sr-only">First Name</label>
-        <input type="text" id="inputFirst" class="form-control" placeholder="First Name" required autofocus/>
+        <input type="text" id="inputFirst" class="form-control" placeholder="First Name" required/>
         <label for="inputLast" class="sr-only">Last Name</label>
-        <input type="text" id="inputLast" class="form-control" placeholder="Last Name" required autofocus/>
+        <input type="text" id="inputLast" class="form-control" placeholder="Last Name" required/>
         <label for="inputEmail" class="sr-only">Email Address</label>
         <input type="email" id="inputEmail" class="form-control" placeholder="Email Address" required/>
-        <label for="inputUserName" class="sr-only">User Name</label>
-        <input type="userName" id="inputUserName" class="form-control" placeholder="User Name" required/>
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required/>
         <label for="inputConfirmPassword" class="sr-only">Confirm Password</label>
         <input type="password" id="inputConfirmPassword" class="form-control" placeholder="Confirm Password" required/>
-        <div class="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me"/> Remember me
-          </label>
-        </div>
+ 
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign Up</button>
       </form>
       </body>
--- client/src/components/Profile/Admin/addSeller.js
+++ client/src/components/Profile/Admin/addSeller.js
@@ -1,50 +1,163 @@
-import React, { Component } from "react";
+import React from "react";
+import CRUDTable, {
+  Fields,
+  Field,
+  CreateForm,
+  UpdateForm,
+  DeleteForm
+} from "react-crud-table";
 
-class addSeller extends Component {
-  render() {
-    return (
-        <div>
-        <div className="landing">
-          <div className="dark-overlay landing-inner text-light">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-12 text-center">
-                  <h1 className="display-3 mb-4">Add a House for Sale</h1>
-                  <p className="lead">
-                    {" "}
-                    Text Here
-                </p>
-                  <hr />
-                
+import "./addSeller.css";
 
+const DescriptionRenderer = ({ field }) => <textarea {...field} />;
 
-                  <br />
-                  <br />
-                  <br />
-                  <br />
-                  <br />
-                  <br />
-                  <div className="dropdown">
-                    <a className="btn btn-secondary dropdown-toggle" href="/" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      Page Views (For Devs Only)
-                     </a>
-                    <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                      <a className="dropdown-item" href="/Admin">Admin Page</a>
-                      <a className="dropdown-item" href="/Representative">Representative Page</a>
-                      <a className="dropdown-item" href="/Buyer">Buyer Page</a>
-                      <a className="dropdown-item" href="/Seller">Seller Page</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-      </div>
-  )
+let Homes = [
+  {
+    id: 1,
+    title: "Home 1",
+    description: "Home Desc sakjdhaiosdjaolijdmamdlnasklda"
+  },
+  {
+    id: 2,
+    title: "Home 2",
+    description: "Home Desc2 dasjodajmodma;mwdnanlmldamldmal"
   }
+];
+
+const SORTERS = {
+  NUMBER_ASCENDING: mapper => (a, b) => mapper(a) - mapper(b),
+  NUMBER_DESCENDING: mapper => (a, b) => mapper(b) - mapper(a),
+  STRING_ASCENDING: mapper => (a, b) => mapper(a).localeCompare(mapper(b)),
+  STRING_DESCENDING: mapper => (a, b) => mapper(b).localeCompare(mapper(a))
+};
+
+const getSorter = data => {
+  const mapper = x => x[data.field];
+  let sorter = SORTERS.STRING_ASCENDING(mapper);
+
+  if (data.field === "id") {
+    sorter =
+      data.direction === "ascending"
+        ? SORTERS.NUMBER_ASCENDING(mapper)
+        : SORTERS.NUMBER_DESCENDING(mapper);
+  } else {
+    sorter =
+      data.direction === "ascending"
+        ? SORTERS.STRING_ASCENDING(mapper)
+        : SORTERS.STRING_DESCENDING(mapper);
   }
 
-  export default addSeller;
+  return sorter;
+};
+
+let count = Homes.length;
+const service = {
+  fetchItems: payload => {
+    let result = Array.from(Homes);
+    result = result.sort(getSorter(payload.sort));
+    return Promise.resolve(result);
+  },
+  create: Home => {
+    count += 1;
+    Homes.push({
+      ...Home,
+      id: count
+    });
+    return Promise.resolve(Home);
+  },
+  update: data => {
+    const Home = Homes.find(t => t.id === data.id);
+    Home.title = data.title;
+    Home.description = data.description;
+    return Promise.resolve(Home);
+  },
+  delete: data => {
+    const Home = Homes.find(t => t.id === data.id);
+    Homes = Homes.filter(t => t.id !== Home.id);
+    return Promise.resolve(Home);
+  }
+};
+
+const styles = {
+  container: { margin: "auto", width: "fit-content" }
+};
+
+const addSeller = () => (
+  <div style={styles.container}>
+    <CRUDTable
+      caption="Homes"
+      fetchItems={payload => service.fetchItems(payload)}
+    >
+      <Fields>
+        <Field name="id" label="Id" hideInCreateForm />
+        <Field name="title" label="Title" placeholder="Title" />
+        <Field
+          name="description"
+          label="Description"
+          render={DescriptionRenderer}
+        />
+      </Fields>
+      <CreateForm
+        title="Home Creation"
+        message="Create a new Home!"
+        trigger="Create Home"
+        onSubmit={Home => service.create(Home)}
+        submitText="Create"
+        validate={values => {
+          const errors = {};
+          if (!values.title) {
+            errors.title = "Please, provide Home's title";
+          }
+
+          if (!values.description) {
+            errors.description = "Please, provide Home's description";
+          }
+
+          return errors;
+        }}
+      />
+
+      <UpdateForm
+        title="Home Update Process"
+        message="Update Home"
+        trigger="Update"
+        onSubmit={Home => service.update(Home)}
+        submitText="Update"
+        validate={values => {
+          const errors = {};
+
+          if (!values.id) {
+            errors.id = "Please, provide id";
+          }
+
+          if (!values.title) {
+            errors.title = "Please, provide Home's title";
+          }
+
+          if (!values.description) {
+            errors.description = "Please, provide Home's description";
+          }
+
+          return errors;
+        }}
+      />
+
+      <DeleteForm
+        title="Home Delete Process"
+        message="Are you sure you want to delete the Home?"
+        trigger="Delete"
+        onSubmit={Home => service.delete(Home)}
+        submitText="Delete"
+        validate={values => {
+          const errors = {};
+          if (!values.id) {
+            errors.id = "Please, provide id";
+          }
+          return errors;
+        }}
+      />
+    </CRUDTable>
+  </div>
+);
+
+export default addSeller;
